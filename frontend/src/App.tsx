e8f1872516d6520016448106533d5f2d3e80b98d
Kartik Turak <kartikturak1@gmail.com>
2024-06-06 11:12:21 +0530

    added logout and Home page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import { Blog } from "./pages/Blog"
 import { BrowserRouter, Route, Routes } from "react-router-dom"
 import {Blogs} from "./pages/Blogs"
 import { Publish } from "./pages/Publish"
+import { Home } from "./pages/Home"
 
 
 
@@ -13,12 +14,13 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+        <Route path="/" element={<Home/>}/>
           <Route path="/signup" element={<Signup/>}/>
           <Route path="/signin" element={<Signin/>}/>
           <Route path="/blog/:id" element={<Blog />} />
           <Route path="/blogs" element={<Blogs/>}/>
           <Route path="/publish" element={<Publish/>}/>
-
+          hello
         </Routes>
       </BrowserRouter>
     </>
--- frontend/src/components/Appbar.tsx
+++ frontend/src/components/Appbar.tsx
@@ -1,17 +1,27 @@
-import { Link } from "react-router-dom"
-import { Avatar } from "./BlogCard"
+import { Link } from "react-router-dom";
+import { Avatar } from "./BlogCard";
 
-export const Appbar = ()=>{
-    return <div className="border-b flex justify-between px-10 py-4 ">
-        <Link to={"/blogs"} className="cursor-pointer">
-            Medium
+export const Appbar = () => {
+  return (
+    <div className="border-b flex justify-between px-10 py-4">
+      <Link to={"/blogs"} className="cursor-pointer text-2xl font-bold">
+        Medium
+      </Link>
+      <div>
+        <Link to={'/'}>
+          <button onClick={(e)=>{
+            localStorage.removeItem("token");
+          }} type="button" className="text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 mr-4" >
+            Logout
+          </button>
         </Link>
-        <div>
-            <Link to={'/publish'}>
-        <button type="button" className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 mr-4">NEW</button>
+        <Link to={'/publish'}>
+          <button type="button" className="text-white bg-green-700 hover:bg-green-800 focus:outline-none focus:ring-4 focus:ring-green-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2 mr-4">
+            NEW
+          </button>
         </Link>
-            <Avatar name="kartik"></Avatar>
-        </div>
-
+        <Avatar name="kartik"></Avatar>
+      </div>
     </div>
+  );
 }
